18fd6d8d88093dc41af8aaeede45117be7bdeb59
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-09-25 22:16:23 -0400

    updating JS and fixing timetable error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
+var currentDate = moment().format('dddd, MMM Do YYYY');
+  $("#currentDay").text(currentDate);
+
+
 //current date
-$(document).ready(function() {
+$(document).ready(function () {
     timeBlocks ();
     renderDream ();
 
-
-  var currentDate = day.js().format('dddd, MMM Do YYYY');
-  $("#currentDay").text(currentDate);
+});
 
 //saveBtn click listener and nearby values in JQuery
-  $(".saveBtn").click(function() {
+  $(".saveBtn").click(function () {
       var text = $(this).siblings(".description").val();
       console.log(text);
       var time = $(this).parent().attr("id");
@@ -20,11 +22,11 @@ $(document).ready(function() {
  
   function timeBlocks() {
   //variable for current time
-    var currentHour = day.js().hour();
+    var currentHour = moment().hour();
 
   //loop over blocks and distinguish current time
     $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id").split("-")[1]);
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
       //check the time and add the classes for background indicators
         if (blockTime < currentHour) { //if time is in the past
             $(this).addClass("past");
@@ -49,5 +51,3 @@ $(document).ready(function() {
   }
 
   renderDream ();
-
-});
